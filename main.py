0eccb867e3530d4c79623ee4cbe2a17cc3800569
Oded Regev <125364181+OdedReg@users.noreply.github.com>
2023-05-29 13:05:01 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 import altair as alt
+import seaborn as sns
 
 df = pd.read_csv('Breast_Cancer.csv')
 
@@ -21,14 +22,29 @@ def build_heatmap():
     st.subheader('Impact of demographic characteristics on the mortality of women with breast cancer in America')
 
     col1, col2 = st.columns(2)
-    options = ['Age', 'Race', 'Marital Status']
+
     with col1:
-        feature1 = build_st_query_for_line_charts("First feature", options)
+        options_feature1 = ['Age', 'Race', 'Marital Status']
+        feature1 = build_st_query_for_line_charts("First feature", options_feature1)
 
     with col2:
-        options2 = ['Age', 'Race', 'Marital Status']
-        options2.remove(feature1)
-        feature2 = build_st_query_for_line_charts("Second feature", options2)
+        options_feature2 = ['Age', 'Race', 'Marital Status']
+        options_feature2.remove(feature1)
+        feature2 = build_st_query_for_line_charts("Second feature", options_feature2)
+
+    # Pivot the dataframe to calculate mortality rates
+    pivot_df = df.pivot_table(index=f'{feature1}', columns=f'{feature2}', values='Status', aggfunc='count')
+
+    # Calculate the mortality rates based on the "Dead" values
+    mortality_rates = pivot_df.apply(lambda x: x['Dead'] / (x['Dead'] + x['Alive']), axis=1)
+
+    # Create a heatmap using seaborn
+    heatmap = sns.heatmap(pivot_df, annot=True, cmap='coolwarm')
+
+    # Display the heatmap in Streamlit
+    st.pyplot(heatmap.figure)
+
+
 
 st.title('Visualization of information - Final Project')
 build_heatmap()
