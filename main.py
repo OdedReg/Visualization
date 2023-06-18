3b6b96d1bba3ec02abab6a0287b0bc770f6be1e8
Oded Regev <125364181+OdedReg@users.noreply.github.com>
2023-06-18 19:02:39 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -288,6 +288,7 @@ def figure3():
 
     col1, col2, col3 = st.columns(3)
     st.subheader("Kaplan-Meier Recovery Estimates and Recovery Time Distribution charts")
+    st.markdown("<h4 style='text-align: right;'>Dashed line represents the average recovery time</h4>")
 
     with col1:
         age_dict = build_st_query_for_ridge_charts(
