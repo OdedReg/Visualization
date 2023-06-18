598e97fb340fbb0878d5a2519c59e07c7729fdc9
Oded Regev <125364181+OdedReg@users.noreply.github.com>
2023-06-18 18:11:10 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -152,10 +152,10 @@ def build_two_y_axis_chart():
         marker=dict(color='lightseagreen')
     ))
 
-    fig.update_layout(height=500, width=800,
+    fig.update_layout(height=500, width=900,
         xaxis=dict(title='<b>Race<b>', title_font=dict(size=22), tickfont=dict(size=16)),
         yaxis=dict(title='<b>Malignancy Rate (%)</b>', title_font=dict(size=22), tickfont=dict(size=16)),
-        yaxis2=dict(title='<b>Average Tumor Size (mm)<b>', overlaying='y', side='right', title_font=dict(size=22)),
+        yaxis2=dict(title='<b>Average Tumor Size (mm)<b>', overlaying='y', side='right', title_font=dict(size=22), tickfont=dict(size=16)),
         barmode='group',
         legend_x=1.2,
         legend_font=dict(size=16),  # Increase the legends' font size
